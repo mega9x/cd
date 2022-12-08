0b9a6bf6fee2298dbee8dac885ca8226555ebdef
Omega <megaTechart@outlook.com>
2022-12-08 20:19:05 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,156 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="app">
+    <div v-if="isGening">
+      {{ent}}
+    </div>
+    <div v-if="!isGening">
+      <a>生成数量: </a>
+      <input v-bind:value="value"/>
+      <button v-on:click="gen">生成</button>
+    </div>
+    <div class="cards" v-for="i in ents" :key="i">
+      <CardCompomentVue :ent="i"></CardCompomentVue>
+    </div>
+  </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardCompomentVue from "./CardCompoment.vue";
+
+const Fakerator = require("fakerator");
+const Chance = require("chance");
+const postApi = "https://randommer.io/random-address";
+const ccardPostApi = "https://dnschecker.org/ajax_files/credit_card_generator.php";
+// const generator = require('creditcard-generator');
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    CardCompomentVue,
+  },
+  name: 'App',
+  async mounted() {
+
+  },
+  data() {
+    return {
+      isGening: false,
+      ent: {},
+      value: 10,
+      ents: [],
+      times: 0,
+    }
+  },
+  methods: {
+    sleep: async function(delay) {
+      return new Promise(r => setTimeout(r, delay));
+    },
+    gen: async function() {
+        this.isGening = true;
+        for(let i = 1; i <= this.value; i++) {
+          let chance = new Chance();
+          let entity;
+          let fakerator = Fakerator("en-CA");
+          fakerator.seed(Math.random());
+          entity = fakerator.entity.user();
+          while(!entity.address.country.includes("Canada")) {
+            fakerator.seed(Date.now());
+            entity = fakerator.entity.user();
+            this.times++;
+            await this.sleep(1);
+            this.ent = entity;
+          }
+          let rndAddress = await this.getAddress();
+          this.ent = entity;
+          let genderGen = this.getGender();
+          let name = chance.name( { nationality: "en", gender: genderGen } )
+          let gender;
+          if(genderGen === "male") {
+            gender = "男";
+          } else {
+            gender = "女";
+          }
+          this.ents.push({
+            fullname: name,
+            nickname: entity.userName,
+            sex: gender,
+            brithday: this.getBirthday(),
+            tempmail: chance.string({
+              length: 8,
+              pool: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789',
+              Symbol: false
+            }) + "@chacuo.com",
+            phone: chance.phone({ formatted: false }),
+            address: rndAddress,
+            ssn: chance.ssn({ dashes: false }),
+          });
+      }
+      this.isGening = false;
+    },
+    objToUrlEncoded: function(obj) {
+      let body = [];
+      for(let prop in obj) {
+        let encodedKey = encodeURIComponent(prop);
+        let encodedVal = encodeURIComponent(obj[prop]);
+        body.push(encodedKey + "=" + encodedVal);
+      }
+      body = body.join("&");
+      return body;
+    },
+    fetchPost: async function(uri, datas) {
+      let response = await fetch(
+        uri, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: datas,
+        }
+      )
+      return response;
+    },
+    getAddress: async function() {
+      let adsData = {
+          number: 1,
+            culture: "en_CA",
+      };
+      let wwwData = this.objToUrlEncoded(adsData);
+      let response = await this.fetchPost(postApi, wwwData);
+      let text = await response.text();
+      text = text.replace('"]', "").replace('["', "");
+      return text;
+    },
+    getCreditCard: async function() {
+      let ccData = {
+        network: "mastercard",
+        quantity: "1",
+      }
+      let data = this.objToUrlEncoded(ccData);
+      let response = await this.fetchPost(ccardPostApi, data);
+      let obj = await response.json();
+      return obj.results;
+    },
+    getBirthday: function() {
+      let chance = new Chance();
+      let yr = this.getRandom(1987, 2001);
+      let mth = this.getRandom(1, 13);
+      let date = chance.date({ month: mth, string: true, american: false });
+      let day = date.split("/")[0];
+      console.log(mth + "-" + day + "-" + yr);
+      return mth + "-" + day + "-" + yr;
+    },
+    getRandom: function (min,max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    getGender: function() {
+      let rnd = this.getRandom(0,2);
+      if(rnd == 1) {
+        return "female";
+      } else {
+        return "male";
+      }
+    }
+  },
 }
 </script>
 
@@ -19,8 +159,8 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 40px;
+  margin-left: 20px;
 }
 </style>
---
+++ src/CardCompoment.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card-container">
+        <p>
+            姓名: {{ ent.fullname }}
+        </p>
+        <p>
+            昵称: {{ ent.nickname }}
+        </p>
+        <p>
+            性别: {{ ent.sex }}  
+        </p>
+        <p>
+            生日: {{ ent.brithday }}
+        </p>
+        <p>
+            电话: {{ ent.phone }}
+        </p>
+        <p>
+            SSN: {{ ent.ssn }}
+        </p>
+        <p>
+            详细地址和邮编: {{ ent.address }}
+        </p>
+        <p>
+            查错网临时邮箱: {{ ent.tempmail }}
+        </p>
+    </div>
+</template>
+  
+<script>
+/* eslint-disable */
+  export default {
+    name: 'Card',
+    props: ['ent'],
+    data() {
+      return {
+      }
+    },
+}
+</script>
+
+<style>
+    .card-container {
+        margin: 15px;
+        padding: 5px;
+        background: rgb(238, 238, 238);
+        border-radius: 5px;
+    }
+    .card-container > p {
+        margin-left: 10px;
+        line-height: 15px;
+    }
+</style>
+  
